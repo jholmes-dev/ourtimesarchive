4282c0d8c98a387bac36d71fdbf5cd0bd5937606
Jeffrey Holmes <coffeman500@gmail.com>
2023-01-21 00:55:59 -0600

    Updated vault page. Added graphics for vault unlocks and filled in some information for pending features

---
+++ resources/views/components/vault-tabs/entries.blade.php
@@ -0,0 +1,5 @@
+<div class="tab-pane fade" id="entries" role="tabpanel" aria-labelledby="entriesTab">
+    
+    <h4 class="text-center">Unlocked entries will appear here after your first vault unlock</h4>
+
+</div>
---
+++ resources/views/components/vault-tabs/unlock.blade.php
@@ -0,0 +1,34 @@
+<div class="tab-pane fade" id="unlock" role="tabpanel" aria-labelledby="unlockTab">
+
+    <p>Ready to crack your vault? Use one of the options below to initiate a <em>vault unlock</em>. Not sure what a vault unlock is? <a href="#">Click here to find out</a>.</p>
+
+    <div class="mt-5 row gy-4 justify-content-around">
+
+        <div class="col-12 col-md-6 unlock-local-wrapper text-center">
+            <div class="unlock-local">
+                <div class="unlock-icon">
+                    <i class="bi bi-person-fill ul1"></i>
+                    <i class="bi bi-person-fill ul2"></i>
+                    <i class="bi bi-person-fill ul3"></i>
+                    <i class="bi bi-person-fill ul4"></i>
+                    <i class="bi bi-laptop ul5"></i>
+                </div>
+                <h5>Local Unlock</h5>
+                <a href="#" class="stretched-link"></a>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 unlock-remote-wrapper text-center">
+            <div class="unlock-remote">
+                <div class="unlock-icon">
+                    <i class="bi bi-person-workspace ul1"></i>
+                    <i class="bi bi-person-workspace ul2"></i>
+                    <i class="bi bi-person-workspace ul3"></i>
+                    <i class="bi bi-person-workspace ul4"></i>
+                </div>
+                <h5>Remote Unlock</h5>
+            </div>
+            <h6>Feature in progress</h6>
+        </div>
+
+    </div>
+</div>
