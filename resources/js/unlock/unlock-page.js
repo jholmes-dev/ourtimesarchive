747a5d5eb83ad79ce2f05f1c90fa7ab60bacc929
Jeffrey Holmes <coffeman500@gmail.com>
2023-04-07 22:49:55 -0500

    More entry unlock work

--- resources/js/unlock/unlock-page.js
+++ resources/js/unlock/unlock-page.js
@@ -2,32 +2,11 @@
  * Unlock view page
  * 
  */
+import { UnlockEntryHandler } from './UnlockEntryHandler';
 
-/**
- * Pull server config variables from the page
- * 
- */
-let authConfig = {
-    'get_entries_url': $('#getEntriesUrl').val(),
-    'unlock_id': $('#unlockId').val()
-};
+let entryHandler = new UnlockEntryHandler();
+init();
 
-
-axios.get(authConfig.get_entries_url, { 
-    'unlock_id': authConfig.unlock_id
-}).then(function(res) {
-    console.log(res.data);
-}).catch(function(e) {
-
-    try {
-        if (e.response.data.message) {
-            displayToast(e.response.data.message, 'exclamation-circle', 'danger');
-        } else {
-            displayToast('An unexpected error has occured. Please try again later.', 'exclamation-circle', 'danger');
-        }
-    } catch (err)
-    {
-        displayToast('An unexpected error has occured. Please try again later.', 'exclamation-circle', 'danger');
-    }
-
-});
+async function init() {
+    let res = await entryHandler.getEntries();
+}
